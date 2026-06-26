2b935b5439639d4d15be176e00d3381f068ae5a2
aes21 <alexander@sadler.org.uk>
2026-06-26 11:55:31 +0100

    add tests

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -2,12 +2,11 @@ import pytest
 import numpy as np
 import pandas as pd
 from unittest.mock import patch, MagicMock
-from forecose import DexcomForecast
 
-@pytest.fixture
-def sample_cgm_data():
-    """Sample CGM data for prior 24 hours."""
-    return np.random.uniform(4.0, 10.0, size=288)
+from pydexcom import Dexcom
+from forecose import DexcomForecast
+from forecose.glucose_forecast import GlucoseForecast
+from forecose.const import CLIP_HIGH, CLIP_LOW
 
 @pytest.fixture
 def mock_timesfm_model():
@@ -16,49 +15,72 @@ def mock_timesfm_model():
     """
     with patch("forecose.forecose.timesfm.TimesFM_2p5_200M_torch.from_pretrained") as mock_model:
         instance = mock_model.return_value
-        mock_point = np.array([[5.5] * 12])
-        mock_quantiles = np.zeros((1, 12, 10))
+        mock_point = np.array([[100] * 12])
 
         # quantiles
-        mock_quantiles[0, :, 0] = 4.0
-        mock_quantiles[0, :, 1] = 4.5
-        mock_quantiles[0, :, 4] = 5.0
-        mock_quantiles[0, :, 7] = 6.5
-        mock_quantiles[0, :, 8] = 7.0
+        mock_quantiles = np.zeros((1, 12, 10))
+        mock_quantiles[0, :, 0] = 80.0
+        mock_quantiles[0, :, 1] = 90.0
+        mock_quantiles[0, :, 4] = 100.0
+        mock_quantiles[0, :, 7] = 110.0
+        mock_quantiles[0, :, 8] = 120.0
 
         instance.forecast.return_value = (mock_point, mock_quantiles)
         yield instance
 
-def test_forecaster_structure(sample_cgm_data, mock_timesfm_model):
-    """Verifies the forecast method returns the correct pd.DataFrame structure."""
+@pytest.fixture
+def mock_dexcom():
+    """Simulates an active pydexcom Dexcom session."""
+    dexcom = MagicMock(spec=Dexcom)
 
-    last_time = pd.Timestamp.now(tz="UTC")
-    interval = pd.Timedelta(minutes=5)
+    readings = []
+    base_time = pd.Timestamp.now(tz="UTC") - pd.Timedelta(hours=24)
 
-    forecaster = DexcomForecast(
-        cgm_history=sample_cgm_data,
-        last_timestamp=last_time,
-        sampling_interval=interval,
-        context_len=288,
-        horizon=12
-    )
+    for i in range(288):
+        mock_reading = MagicMock()
+        mock_reading.datetime = base_time - pd.Timedelta(minutes=5*i)
+        mock_reading.value = 105
+        readings.append(mock_reading)
 
-    df = forecaster.forecast()
+    dexcom.get_glucose_readings.return_value = readings[::-1]
+    return dexcom
+
+@pytest.fixture
+def mock_dexcom_empty():
+    """Simulates a failed API called or empty session data."""
+    dexcom = MagicMock(spec=Dexcom)
+    dexcom.get_glucose_readings.return_value = []
+
+    return dexcom
+
+
+def test_forecast_structure(mock_dexcom, mock_timesfm_model):
+    """Verifies the forecast method returns the correct subclass structure."""
+    forecaster = DexcomForecast()
+    df = forecaster.get_forecast(mock_dexcom)
+
+    assert isinstance(df, GlucoseForecast)
 
     assert len(df) == 12
 
     assert list(df.columns) == ["timestamp", "predicted_glucose", "q10", "q25", "q50", "q75", "q90"]
 
-    assert df["timestamp"].iloc[0] == last_time + interval
-    assert df["timestamp"].iloc[1] == last_time + (2 * interval)
-
-def test_forecaster_empty_history():
+def test_forecaster_empty_history(mock_dexcom_empty, mock_timesfm_model):
     """Verifies the model blocks execution if no data is passed from the Dexcom Share API."""
-    forecaster = DexcomForecast(
-        cgm_history=np.array([]),
-        last_timestamp=pd.Timestamp.now(tz="UTC"),
-        sampling_interval=pd.Timedelta(minutes=5)
-    )
+    forecaster = DexcomForecast()
 
-    with pytest.raises(ValueError, match="No historical data provided"):
-        forecaster.forecast()
+    with pytest.raises(RuntimeError, match="No readings returned from Dexcom Share API."):
+        forecaster.get_forecast(mock_dexcom_empty)
+
+def test_mmol_l_conversion(mock_dexcom, mock_timesfm_model):
+    """Verifies glucose value conversion on the subcluss structure output."""
+    forecaster = DexcomForecast()
+
+    df = forecaster.get_forecast(mock_dexcom)
+    df_mmol = df.mmol_l
+
+    assert isinstance(df_mmol, GlucoseForecast)
+
+    assert df_mmol["predicted_glucose"].iloc[0] < 22.0
+
+    assert not df["predicted_glucose"].equals(df_mmol["predicted_glucose"])
